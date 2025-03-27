de6b0f6906a6ab45e33ca617fb3b362a6f5b660e
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-03-27 22:25:13 +0530

    Header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
         </div>
 
             <div className="col-span-10 text-center mt-1">
-            <input type="text" className="w-1/2 border border-gray-400 p-1"/>
-            <button className="border border-gray-400 p-1">Search</button>
+            <input type="text" className="w-1/2 border border-gray-400 p-1 rounded-l-full"/>
+            <button className="border border-gray-400 py-1 px-4 rounded-r-full bg-gray-100">🔍</button>
             </div>
 
             <div className="col-span-1">
